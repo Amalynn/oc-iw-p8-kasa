4f95ae9c9523e5ed3b3575580c628524910cfd89
Amalynn <amandine.ebner@gmail.com>
2024-02-23 09:27:46 +0100

    Header integration

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,39 @@
+import { NavLink, Link } from 'react-router-dom';
+import logo from '../assets/logo/logo-colors.svg';
+
 export default function Header() {
-    return <h1>Ceci est le Header</h1>;
+    return (
+        <div className="main-container">
+            <header className="header">
+                <Link to="/">
+                    <img src={logo} alt="Logo de Kasa" />
+                </Link>
+
+                <nav className="navbar">
+                    <ul>
+                        <li>
+                            <NavLink
+                                to="/"
+                                className={({ isActive }) =>
+                                    isActive ? 'active' : ''
+                                }
+                            >
+                                Accueil
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink
+                                to="/about"
+                                className={({ isActive }) =>
+                                    isActive ? 'active' : ''
+                                }
+                            >
+                                A propos
+                            </NavLink>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+    );
 }
