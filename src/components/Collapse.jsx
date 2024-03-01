5e0f4bd4322dead8b099cc259b8772b879a5a996
Amalynn <amandine.ebner@gmail.com>
2024-03-01 10:48:50 +0100

    Integration of the About page.

---
+++ src/components/Collapse.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react';
+import { useRef } from 'react';
+import PropTypes from 'prop-types';
+import arrowIcon from '../assets/icons/arrow-icon.svg';
+
+export default function Collapse({ title, children }) {
+    const ref = useRef(null);
+    const [contentHeight, setContentHeight] = useState(0);
+    const [isOpen, setIsOpen] = useState(false);
+
+    useEffect(() => {
+        if (isOpen) {
+            setContentHeight(ref.current.scrollHeight);
+        } else {
+            setContentHeight(0);
+        }
+    }, [isOpen]);
+
+    return (
+        <section className="collapse">
+            <div className="collapse-header">
+                <h2>{title}</h2>
+                <button
+                    onClick={() => setIsOpen(!isOpen)}
+                    aria-expanded={isOpen ? true : false}
+                >
+                    <img
+                        src={arrowIcon}
+                        className={isOpen ? 'rotate' : ''}
+                        alt="Arrow icon"
+                    />
+                </button>
+            </div>
+            <div
+                ref={ref}
+                className={`collapse-body ${isOpen ? 'open' : ''}`}
+                style={{ height: `${contentHeight}px` }}
+            >
+                {children}
+            </div>
+        </section>
+    );
+}
+
+Collapse.propTypes = {
+    title: PropTypes.string.isRequired,
+    children: PropTypes.node.isRequired,
+};
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,50 @@
 import bannerAbout from '../assets/images/banner-about.jpg';
 import Banner from '../components/Banner.jsx';
+import Collapse from '../components/Collapse.jsx';
 
 export default function About() {
     const imageTitle = 'Bannière de la page "A propos"';
     return (
         <>
             <Banner image={bannerAbout} imageTitle={imageTitle} />
+
+            <div className="collapse-about">
+                <Collapse title="Fiabilité">
+                    <p>
+                        Les annonces postées sur Kasa garantissent une fiabilité
+                        totale. Les photos sont conformes aux logements, et
+                        toutes les informations sont régulièrement vérifiées par
+                        nos équipes.
+                    </p>
+                </Collapse>
+                <Collapse title="Respect">
+                    <p>
+                        La bienveillance fait partie des valeurs fondatrices de
+                        Kasa. Tout comportement discriminatoires ou de
+                        perturbation du voisinage entraînera une exclusion de
+                        notre plateforme.
+                    </p>
+                </Collapse>
+                <Collapse title="Service">
+                    <p>
+                        La bienveillance fait partie des valeurs fondatrices de
+                        Kasa. Tout comportement discriminatoires ou de
+                        perturbation du voisinage entraînera une exclusion de
+                        notre plateforme.
+                    </p>
+                </Collapse>
+                <Collapse title="Sécurité">
+                    <p>
+                        La sécurité est la priorité de Kasa. Aussi bien pour nos
+                        hôtes que pour les voyageurs, chaque logement correspond
+                        aux critères de sécurité établis par nos services. En
+                        laissant une note aussi bien à l&apos;hôte qu&apos;au
+                        locataire, cela permet à nos équipes de vérifier que les
+                        standards sont bien respectés. Nous organisonségalement
+                        des ateliers sur la sécurité domestique pour nos hôtes.
+                    </p>
+                </Collapse>
+            </div>
         </>
     );
 }
